b4e95c7151dca10ea8c010fc1fcf68d4527695e7
choihamin <jason4284@korea.ac.kr>
2021-07-24 16:22:49 +0900

    성공했습니다.4

--- Backend.py
+++ Backend.py
@@ -19,18 +19,41 @@ connect = psycopg2.connect(database=url.path[1:],
 cur = connect.cursor()
 cur.execute("select * from customer where customer_id='jason4284' and password='123456789a!' and Did='2017170843'")
 data = cur.fetchall()
+print(data)
 
-@app.route('/CheckLogin')
+@app.route('/CheckLogin', methods=['GET'])
 def CheckLogin():
     id = request.args.get('Id')
     pw = request.args.get('Password')
-    Did = request.args.get('DeviceId')
-    print(id, pw, Did)
-    try:
-        cur.execute("select * from customer where customer_id='{}' and password='{}' and Did='{}'".format(id, pw, Did))
+    print(id, pw)
+    cur.execute("select * from customer where customer_id='{}' and password='{}'".format(id, pw))
+    data = cur.fechall()
+    if len(data) != 1:
         return jsonify({'result_code': 1})
-    except:
+    else:
         return jsonify({'result_code': 0})
+
+@app.route('/GetMemberInfo', methods=['GET'])
+def GetMemberInfo():
+    id = request.args.get('Id')
+    cur.execute("select * from customer car_model_name where customer_id='{}'".format(id))
+
+
+@app.route('/SetSignUpInfo', methods=['POST'])
+def SetSignUpInfo():
+    id = request.args.get('Id')
+    pw = request.args.get('Password')
+    phone = request.args.get('Phone')
+    name = request.args.get('Name')
+    car_company = request.args.get('Car_company')
+
+    cur.execute("insert into customer values('{}','{}','{}')".format(id, pw, name))
+    cur.execute("insert into phone_cus values('{}','{}')".format(id, phone))
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(Debug=True, host='0.0.0.0')
