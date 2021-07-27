d5521ca1dfff69180aa9ca72e6b2720925db792a
choihamin <jason4284@korea.ac.kr>
2021-07-27 17:23:40 +0900

    성공했습니다.4

--- Backend.py
+++ Backend.py
@@ -3,6 +3,9 @@ import urllib.parse as up
 import psycopg2, datetime
 from flask import Flask, render_template, request, redirect, session, jsonify
 from flask_restx import Api, Resource
+from mapboxgl.utils import df_to_geojson
+import json
+import pandas as pd
 
 
 app=Flask(__name__)
@@ -17,7 +20,7 @@ connect = psycopg2.connect(database=url.path[1:],
                         host=url.hostname,
                         port=url.port)
 cur = connect.cursor()
-cur.execute("select * from customer where customer_id='jason4284' and password='123456789a!' and Did='2017170843'")
+cur.execute("select * from Station")
 data = cur.fetchall()
 print(data)
 
@@ -33,24 +36,88 @@ def CheckLogin():
     else:
         return jsonify({'result_code': 0})
 
-@app.route('/GetMemberInfo', methods=['GET'])
+@app.route('/GetMemberInfo', methods=['POST'])
 def GetMemberInfo():
     id = request.args.get('Id')
-    cur.execute("select * from customer car_model_name where customer_id='{}'".format(id))
+    cur.execute("select * from customer where customer_id='{}'".format(id))
+    data = cur.fetchall()
+    name = data[0][2]
+    car_model = data[0][3]
+
+    return jsonify({'name': name,
+                    'car_models': car_model})
+
+@app.route('/GetCarInfo', methods=['POST'])
+def GetChargeResult():
+    id = request.args.get('Id')
+    cur.execute("select * from ServiceReservation where customer_id='{}".format(id))
+    data = cur.fetchall()[-1]
+    current_capacity = '몰라'
+    seq_reserve = data[0]
+    reserve_type = data[6]
+    reserve_time = data[3]
+    finish_time = data[4]
+    min_capacity = data[5]
+
+    return jsonify({
+        'current_capacity': current_capacity,
+        'seq_reserve': seq_reserve,
+        'reserve_type': reserve_type,
+        'reserve_time': reserve_time,
+        'finish_time': finish_time,
+        'min_capacity': min_capacity
+    })
 
 
 @app.route('/SetSignUpInfo', methods=['POST'])
 def SetSignUpInfo():
+    name = request.args.get('Name')
     id = request.args.get('Id')
     pw = request.args.get('Password')
-    phone = request.args.get('Phone')
-    name = request.args.get('Name')
-    car_company = request.args.get('Car_company')
+    car_model = request.args.get('Car_model')
+    try:
+        cur.execute("insert into customer values('{}','{}','{}','{}')".format(id, pw, name, car_model))
+        connect.commit()
+        return jsonify({'result_code': 1})
+    except:
+        return jsonify({'result_code': 0})
 
-    cur.execute("insert into customer values('{}','{}','{}')".format(id, pw, name))
-    cur.execute("insert into phone_cus values('{}','{}')".format(id, phone))
+@app.route('/GetCarCompanyInfo', methods=['POST'])
+def GetCarCompanyInfo():
+    dict_ = {}
+    cur.execute("select distinct manufacturer from carmodel")
+    data = cur.fetchall()
 
 
+
+@app.route('/GetCarModelInfo', methods=['POST'])
+def GetCarModelInfo():
+    dict_ = {}
+    company = request.args.get('Car_company')
+    cur.execute("select car_model from CarModel where manufacturer='{}'".format(company))
+    data = cur.fetchall()
+    for i in range(len(data)):
+        dict_["model_{}".format(i)] = data[i][0]
+    return jsonify(dict_)
+
+@app.route('/GetStationInfo', methods=['POST'])
+def GetStationInfo():
+    dict_ = {}
+    cur.execute("select station_id, station_name, slow_charger, fast_charger, dx, dy")
+    data = cur.fetchall()
+    data = pd.DataFrame(data, columns=['station_id', 'station_name', 'slow_charger', 'fast_charger', 'dx', 'dy'])
+    geo_data = df_to_geojson(
+        df=data,
+        properties=['station_id', 'station_name', 'slow_charger', 'fast_charger'],
+        lat='dx',
+        lng='dy',
+        precision=5,
+        filename='station.geojson'
+    )
+    path = 'station.geojson'
+    with open(path) as f:
+        data = json.loads(f.read())
+    return data
 
 
 
