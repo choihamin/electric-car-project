6d9b62b42c85f1af497bc8a640240c6ce1f0d776
choihamin <jason4284@korea.ac.kr>
2021-08-09 11:42:18 +0900

    성공했습니다.4

--- Backend.py
+++ Backend.py
@@ -353,8 +353,11 @@ def SetReserveInfo():
 
 sched = BackgroundScheduler()
 sched.start()
-sched.add_job(return_supp, 'cron', args=['HourData'], minute='5', second='30', id="test_1")
-sched.add_job(return_supp, 'cron', args=['LpData'], minute='*/15', second='0', id="test_2")
+sched.add_job(return_supp, 'cron', args=['HourData'], minute='12', second='30', id="test_1")
+sched.add_job(return_supp, 'cron', args=['LpData'], minute='5', second='0', id="test_2")
+sched.add_job(return_supp, 'cron', args=['LpData'], minute='20', second='0', id="test_2")
+sched.add_job(return_supp, 'cron', args=['LpData'], minute='35', second='0', id="test_2")
+sched.add_job(return_supp, 'cron', args=['LpData'], minute='50', second='0', id="test_2")
 sched.add_job(prophet_1hour, 'cron', minute='10', second='0', id="test_3")
 
 if __name__ == "__main__":
